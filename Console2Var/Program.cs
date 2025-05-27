fde53ec8bda35b355231a230dd2d9241f8c9d14b
212321 <212321@DESKTOP-NJ0C7QV>
2025-05-27 16:50:32 +0300

    Добавьте файлы проекта.

---
+++ Console2Var/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2Var
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = { 5, 2, 9, 1, 5, 6 };
+            int maxNumber = MaxFinder.GetMax(numbers);
+            Console.WriteLine($"Максимальное число: {maxNumber}");
+            Console.ReadKey();
+        }
+    }
+
+    public class MaxFinder
+    {
+        public static int GetMax(int[] numbers)
+        {
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                    max = numbers[i];
+            }
+            return max;
+        }
+    }
+
+}
+
---
+++ WPF2Var/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF2Var
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RasschetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int ves = Convert.ToInt32(VesTb.Text);
+            int rost = Convert.ToInt32(RostTb.Text);
+
+            double a = ves / ((rost /100) ^ 2);
+            restb.Text = Convert.ToString(a);
+
+            MessageBox.Show("Нормальный вес");
+            return;
+        }
+
+    }
+}
